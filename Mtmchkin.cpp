237b5dea148b3739751ee5409208f173c6755cfd
Dani <dani@commenda.io>
2023-01-11 18:44:54 +0200

    sldkj

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -47,6 +47,7 @@ Mtmchkin::Mtmchkin(const std::string &fileName) {
             m_cards.push(Mana());
         }
         else {
+            std::cout << "we kdslkdjflskjdflksjd" << std::endl;
             throw DeckFileFormatError(lineNumber);
         }
         lineNumber++;
