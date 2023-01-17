cd1fafcc15158c0aa16775220a3fa724e04d2c00
Dani <dani@commenda.io>
2023-01-17 15:33:03 +0200

    sdkj

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -131,6 +131,7 @@ void Mtmchkin::printLeaderBoard() const {
 
     int currentPlace = m_winnersCount;
     for (Player player : m_players) {
+        std::cout << player.getPlace() << std::endl;
         if (player.getPlace() == -1) {
             printPlayerLeaderBoard(++currentPlace, player);
             std::cout << "printing player" << std::endl;
