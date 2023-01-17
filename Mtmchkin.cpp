9e4927ad920e227243f02c356c0af436636b9cc7
Dani <dani@commenda.io>
2023-01-17 13:49:11 +0200

    sdkj

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -21,6 +21,7 @@ const int maxWordLength = 16;
 Mtmchkin::Mtmchkin(const std::string &fileName) {
     std::cout << "yooo" << std::endl;
     std::ifstream file(fileName);
+        std::cout << "yooo" << std::endl;
     if (!file) {
 		throw DeckFileNotFound();
     }
